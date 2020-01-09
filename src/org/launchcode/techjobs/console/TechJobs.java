a06ef4b9971f465e5c5450c77348c3b4cb6af6bd
las5h4 <loganasmalley@gmail.com>
2020-01-09 13:46:46 -0600

    Configured method printJobs()

--- src/org/launchcode/techjobs/console/TechJobs.java
+++ src/org/launchcode/techjobs/console/TechJobs.java
@@ -3,6 +3,7 @@ package org.launchcode.techjobs.console;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
+import java.util.Map;
 
 /**
  * Created by LaunchCode
@@ -110,7 +111,13 @@ public class TechJobs {
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
-
-        System.out.println("printJobs is not implemented yet");
+        for (HashMap<String, String> job : someJobs){
+            System.out.println("*****");
+            for (Map.Entry<String, String> info : job.entrySet()){
+                System.out.println(info.getKey() + ": " + info.getValue());
+            }
+            System.out.println("*****");
+        }
+//        System.out.println("printJobs is not implemented yet");
     }
 }
